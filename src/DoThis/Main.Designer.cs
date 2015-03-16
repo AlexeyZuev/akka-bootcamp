7d664b20e07f38d0e4e44b42c52488dc8d559c9b
AlexeyZuev <zuev.alexey@gmail.com>
2015-03-16 18:20:55 +0200

    2.3

--- src/DoThis/Main.Designer.cs
+++ src/DoThis/Main.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace ChartApp
+﻿using Akka.Actor;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+namespace ChartApp
 {
     partial class Main
     {
@@ -28,52 +31,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.addSeriesButton = new System.Windows.Forms.Button();
+            this.cpuButton = new System.Windows.Forms.Button();
+            this.memoryButton = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.diskButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // sysChart
             // 
-            chartArea1.Name = "ChartArea1";
-            this.sysChart.ChartAreas.Add(chartArea1);
+            chartArea3.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea3);
             this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend1.Name = "Legend1";
-            this.sysChart.Legends.Add(legend1);
+            legend3.Name = "Legend1";
+            this.sysChart.Legends.Add(legend3);
             this.sysChart.Location = new System.Drawing.Point(0, 0);
+            this.sysChart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sysChart.Name = "sysChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.sysChart.Series.Add(series1);
-            this.sysChart.Size = new System.Drawing.Size(684, 446);
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.sysChart.Series.Add(series3);
+            this.sysChart.Size = new System.Drawing.Size(912, 549);
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
-            // addSeriesButton
+            // cpuButton
             // 
-            this.addSeriesButton.Location = new System.Drawing.Point(576, 373);
-            this.addSeriesButton.Name = "addSeriesButton";
-            this.addSeriesButton.Size = new System.Drawing.Size(75, 23);
-            this.addSeriesButton.TabIndex = 1;
-            this.addSeriesButton.Text = "Add series";
-            this.addSeriesButton.UseVisualStyleBackColor = true;
-            this.addSeriesButton.Click += new System.EventHandler(this.addSeriesButton_Click);
+            this.cpuButton.Location = new System.Drawing.Point(764, 401);
+            this.cpuButton.Name = "cpuButton";
+            this.cpuButton.Size = new System.Drawing.Size(122, 23);
+            this.cpuButton.TabIndex = 1;
+            this.cpuButton.Text = "CPU (on)";
+            this.cpuButton.UseVisualStyleBackColor = true;
+            this.cpuButton.Click += new System.EventHandler(this.cpuButton_Click);
+            // 
+            // memoryButton
+            // 
+            this.memoryButton.Location = new System.Drawing.Point(764, 430);
+            this.memoryButton.Name = "memoryButton";
+            this.memoryButton.Size = new System.Drawing.Size(122, 23);
+            this.memoryButton.TabIndex = 2;
+            this.memoryButton.Text = "Memory (off)";
+            this.memoryButton.UseVisualStyleBackColor = true;
+            this.memoryButton.Click += new System.EventHandler(this.memoryButton_Click);
+            // 
+            // diskButton
+            // 
+            this.diskButton.Location = new System.Drawing.Point(764, 459);
+            this.diskButton.Name = "diskButton";
+            this.diskButton.Size = new System.Drawing.Size(122, 23);
+            this.diskButton.TabIndex = 3;
+            this.diskButton.Text = "Disk (off)";
+            this.diskButton.UseVisualStyleBackColor = true;
+            this.diskButton.Click += new System.EventHandler(this.diskButton_Click);
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 446);
-            this.Controls.Add(this.addSeriesButton);
+            this.ClientSize = new System.Drawing.Size(912, 549);
+            this.Controls.Add(this.diskButton);
+            this.Controls.Add(this.memoryButton);
+            this.Controls.Add(this.cpuButton);
             this.Controls.Add(this.sysChart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "System Metrics";
             this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,7 +114,10 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
-        private System.Windows.Forms.Button addSeriesButton;
+        private System.Windows.Forms.Button cpuButton;
+        private System.Windows.Forms.Button memoryButton;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button diskButton;
     }
 }
 
